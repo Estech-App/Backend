4d6a55764392811fe8b6f202c858a1f28472c681
Iván Tortosa <shadowheroe03@gmail.com>
2024-02-12 18:58:03 +0100

    initial commit

---
+++ src/main/java/com/estech/EstechAppBackend/EstechAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.estech.EstechAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstechAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstechAppBackendApplication.class, args);
+	}
+
+}
